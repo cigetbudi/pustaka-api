94eeff208ff9ee04858d7e0cec31085077b77e1b
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-06-02 00:17:33 +0700

    divide the handler and router

--- main.go
+++ main.go
@@ -9,11 +9,22 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Sigit Budi",
-			"bio":  "miwon",
-		})
-	})
+	router.GET("/", rootHandler)
+
+	router.GET("/hello", helloHandler)
 	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Sigit Budi",
+		"bio":  "miwon",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name":    "Sigit Budi",
+		"message": "HALO SEMUA",
+	})
 }
